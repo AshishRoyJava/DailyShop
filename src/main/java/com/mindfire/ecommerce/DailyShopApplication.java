204ccc0426c114d76197ab5497f648afd6e5a567
Ashish Roy <ashish.roy@mindfiresolutions.com>
2016-08-01 15:54:37 +0530

    Initial Setup of Spring Project

---
+++ src/main/java/com/mindfire/ecommerce/DailyShopApplication.java
@@ -0,0 +1,12 @@
+package com.mindfire.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyShopApplication.class, args);
+	}
+}
